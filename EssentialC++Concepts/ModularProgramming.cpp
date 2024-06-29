90c80cd60ff06c331f4da43f6a39d06b3ac75381
Patlolla Sravanthi <63411144+sravanthi194@users.noreply.github.com>
2024-06-29 18:36:44 +0530

    Create ModularProgramming

---
+++ EssentialC++Concepts/ModularProgramming.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int area(int length,int breadth)
+{
+ return length*breadth;
+}
+
+int perimeter(int length,int breadth)
+{
+return (0.5*length*breadth);
+}
+//Modular programming everything inside main()
+int main()
+{
+    int length,breadth;
+    cin>>length>>breadth;
+    
+    //area 
+      int a=area(length,breadth);
+    cout<<a<<endl;
+    
+    //perimeter
+    int p= perimeter(length,breadth);
+    cout<<p<<endl;
+}
